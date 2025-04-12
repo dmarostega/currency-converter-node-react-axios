801f445527e1ce4dfe44de98a45f4de675c9e36e
dmarostega@gmail.com <dmarostega@gmail.com>
2025-04-12 00:23:44 -0300

    fix(sistema): resolve qualquer rota carregando home

--- back-end/src/routes/homeRoutes.js
+++ back-end/src/routes/homeRoutes.js
@@ -2,6 +2,7 @@ const Router = require('express');
 const router = Router();
 const homeController = require('../controllers/homeController')
 
-router.use('/', homeController.getHome);
+router.get('/', homeController.getHome);
+
 module.exports = router;
 
