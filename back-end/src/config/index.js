21f8d7e9b7813fc659feb83c826fcd5bd95db547
dmarostega@gmail.com <dmarostega@gmail.com>
2025-04-12 01:04:29 -0300

    refactor(config): remove chave

--- back-end/src/config/index.js
+++ back-end/src/config/index.js
@@ -1,6 +1,9 @@
 
-const EXCHANGE_API_KEY = '154a81d8c4d2272fb0eb96b7569185d6';
-
+const EXCHANGE_API_KEY = 'access_code';
+/**
+ * Pode ser adquirida em:
+ *      https://exchangerate.host/ 
+ */
 function getExchangeApiKey () {
     return EXCHANGE_API_KEY;
 }
